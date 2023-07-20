643410bb8cd8c246eff1c8c0166f1c2795e1ca67
ElizeuFragas <elizeu.fragas@itec.ufpa.br>
2023-07-20 18:31:45 -0300

    fix warning on global vim variable and set auto setup lsp's

--- lua/core/plugins_config/lsp_config.lua
+++ lua/core/plugins_config/lsp_config.lua
@@ -21,13 +21,30 @@ require("mason-lspconfig").setup_handlers {
     -- Next, you can provide a dedicated handler for specific servers.
     -- For example, a handler override for the `rust_analyzer`:
 }
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
 --local capabilities = require('cmp_nvim_lsp').default_capabilities()
 -- After setting up mason-lspconfig you may set up servers via lspconfig
---
---require("lspconfig").cssls.setup{
---   capabilities = capabilities,
---	on_attach = on_attach,
---}
 --
 --require("lspconfig").html.setup{
 --   capabilities = capabilities,
