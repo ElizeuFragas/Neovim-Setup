61ff505a7ea2a6b427f59d2a9510cc3c11772061
ElizeuFragas <elizeu.fragas@itec.ufpa.br>
2023-07-20 18:34:34 -0300

    improvement to use nvim api

--- lua/core/sets.lua
+++ lua/core/sets.lua
@@ -1,14 +1,18 @@
-vim.opt.number = true
-vim.opt.autowrite = true
-vim.opt.cursorline = true
-vim.opt.autoread = true
-vim.opt.expandtab = true
-vim.opt.autoindent = true
-vim.opt.tabstop = 3
-vim.opt.shiftwidth = 4
-vim.opt.encoding = 'utf-8'
-vim.opt.title = true
-vim.opt.clipboard='unnamedplus'
-vim.api.nvim_set_option("clipboard","unnamed")
-vim.opt.termguicolors = true
-vim.cmd[[set guicursor=]]
+local opt = vim.opt
+local cmd = vim.cmd
+local set_option = vim.api.nvim_set_option
+
+opt.number = true
+opt.autowrite = true
+opt.cursorline = true
+opt.autoread = true
+opt.expandtab = true
+opt.autoindent = true
+opt.tabstop = 3
+opt.shiftwidth = 4
+opt.encoding = 'utf-8'
+opt.title = true
+opt.termguicolors = false
+opt.clipboard='unnamedplus'
+set_option("clipboard","unnamed")
+cmd[[set guicursor=]]
